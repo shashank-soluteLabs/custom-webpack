6e5bc296649866afe6eda9795c8a33a0fd950f65
shashank-soluteLabs <shashank.s@solutelabs.com>
2022-11-09 11:20:39 +0530

    initial set-up for webpack is done

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React, { useState } from "react";
+import { hot } from "react-hot-loader";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  const handleClick = () => {
+    setCount((prevState) => prevState + 1);
+  };
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>Counter for count</p>
+        <p>{count}</p>
+        <button onClick={handleClick}>Click Me</button>
+      </header>
+    </div>
+  );
+}
+
+export default hot(module)(App);
---
+++ src/index.js
@@ -0,0 +1,47 @@
+// import React from "react";
+// import ReactDOM from "react-dom";
+// import App from "./App";
+// import reportWebVitals from "./reportWebVitals";
+
+// const rootId = document.getElementById("root");
+
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+//   rootId
+// );
+
+// // If you want to start measuring performance in your app, pass a function
+// // to log results (for example: reportWebVitals(console.log))
+// // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// reportWebVitals();
+// if (module.hot && process.env.NODE_ENV === "development") {
+//   module.hot.accept("./App", () => {
+//     const NextApp = require("./App").default;
+//     ReactDOM.render(
+//       <React.StrictMode>
+//         <App />
+//       </React.StrictMode>,
+//       rootId
+//     );
+//   });
+// }
+
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+
+function renderApp() {
+  ReactDOM.render(
+    <React.StrictMode>
+      <App/>
+    </React.StrictMode>,
+    document.getElementById("root")
+  );
+}
+renderApp();
+if (process.env.NODE_ENV === "development" && module.hot) {
+  module.hot.accept();
+}
+// serviceWorker.register();
